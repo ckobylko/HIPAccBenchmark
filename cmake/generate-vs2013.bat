9960d4be98348c4dfd03f7e902bacc72d60fabfc
Christof Kobylko <ckobylko@users.noreply.github.com>
2015-07-22 15:05:16 +0200

    Removed hard-coded HIPAcc installation path from the CMakeLists

--- cmake/generate-vs2013.bat
+++ cmake/generate-vs2013.bat
@@ -1,6 +1,10 @@
 @echo off
-REM Enter the path to your CMake installation here
-SET CMAKE_EXECUTABLE="C:\Program Files (x86)\CMake 2.8\bin\cmake.exe"
+REM Enter the path to your CMake executable here
+SET CMAKE_EXECUTABLE="C:/Program Files (x86)/CMake 2.8/bin/cmake.exe"
+REM Enter the path to your HIPAcc installation here
+SET IPACC_INSTALL_PATH="Z:/Projects/Hipacc_install/"
+
+SET CMAKE_ARGUMENTS=-DHIPACC_INSTALL_PATH:PATH="%IPACC_INSTALL_PATH%"
 
 ECHO.
 ECHO Generate the 32-bit solution ...
@@ -8,7 +12,7 @@ ECHO.
 
 mkdir "%~dp0..\vs2013_cmake_x86"
 cd "%~dp0..\vs2013_cmake_x86"
-%CMAKE_EXECUTABLE% %~dp0 -G "Visual Studio 12"
+%CMAKE_EXECUTABLE% %~dp0 %CMAKE_ARGUMENTS% -G "Visual Studio 12"
 if NOT ["%errorlevel%"]==["0"] (
  pause 
 )
@@ -25,7 +29,7 @@ ECHO.
 
 mkdir "%~dp0\..\vs2013_cmake_x64"
 cd "%~dp0\..\vs2013_cmake_x64"
-%CMAKE_EXECUTABLE% %~dp0 -G "Visual Studio 12 Win64"
+%CMAKE_EXECUTABLE% %~dp0 %CMAKE_ARGUMENTS% -G "Visual Studio 12 Win64"
 if NOT ["%errorlevel%"]==["0"] (
  pause 
 )
