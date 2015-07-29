2ff69009656fa2f03a5497c33e4bbfef234c4c49
Christof Kobylko <ckobylko@users.noreply.github.com>
2015-07-29 15:09:33 +0200

    Storing comparison image of the Mandelbrot test runner

--- src/Mandelbrot/MandelbrotDefines.h
+++ src/Mandelbrot/MandelbrotDefines.h
@@ -21,6 +21,7 @@
       CREATE_IMAGE( OutputImageType, spCompImage ); \
       itHandler->Launch( spCompImage, x0, y0, (x1 - x0) / spOutputImage->GetWidth(), (y1 - y0) / spOutputImage->GetHeight(), count ); \
       printf( "\n%8s\"%10s\"  <-->  \"%14s\"  =  %5.3e", "", strRefHandlerName.c_str(), itHandler->GetName().c_str(), spOutputImage->MaxDeviation( *spCompImage ) ); \
+      ImageIO::SaveImage( "Comp_Mandelbrot_" #__PixelType ".xsc", *spCompImage ); \
     } \
   } \
   ImageIO::SaveImage( "Out_Mandelbrot_" #__PixelType ".xsc", *spOutputImage ); \
